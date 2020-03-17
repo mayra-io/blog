f83fe161627dfb06dbf1d3bfefc8ddc3be314053
Mayra C <mayraacm24@gmail.com>
2020-03-17 16:44:12 -0700

    add user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  protect_from_forgery prepend: true
+
+  def user_signed_in?
+  end
+
+  def current_user
+  end
+
+  def user_session
+  end
 end
